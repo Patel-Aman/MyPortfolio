b478bc1ceee71141da94f5872bbf33a1db13fec0
Aman Patel <patel.26@iitj.ac.in>
2023-02-01 16:26:41 +0530

    Updated contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,19 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+if (isset($_POST['name']) && isset($_POST['email']) && isset($_POST['message'])) {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $subject = $_POST['subject'];
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'patelaman1653@gmail.com';
+  $to = 'patelaman1653@gmail.com';
+  $message = "Name: $name\nEmail: $email\nMessage:\n$message";
+  $headers = "From: $email\n";
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+  if (mail($to, $subject, $message, $headers)) {
+    echo 'Your message was sent successfully!';
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    echo 'There was an error sending your message. Please try again later.';
   }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
+} else {
+  echo 'Please fill out all required fields.';
+}
